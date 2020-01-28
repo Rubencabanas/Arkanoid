f806b4a768407ba1f5c2afb8d7d7e18579cccf6b
Rubén Cabañas Romero <rubencabanas2001@gmail.com>
2020-01-28 11:13:07 +0100

    subida version 1

---
+++ Pelota.java
@@ -0,0 +1,73 @@
+package version1;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Pelota extends Actor {
+
+	private static Pelota instance = null;
+	protected int vx = 4;
+	protected int vy = 4;
+
+	public Pelota() {
+		super();
+		x = 100;
+		y = 100;
+	}
+
+	public void act() {
+		super.act();
+
+		this.x += this.vx;
+		if (this.x < 0 || this.x > (Arkanoid.getInstance().getWidth() - this.getWidth())) {
+			vx = -vx;
+		}
+
+		this.y += this.vy;
+		if (this.y < 0 || this.y > (Arkanoid.getInstance().getHeight() - this.getHeight())) {
+			vy = -vy;
+		}
+
+		System.out.println("act Pelota x: " + x + " y:" + y);
+	}
+	public void collisionWith(Actor actorCollisioned) {
+		super.collisionWith(actorCollisioned);
+		// Debo comprobar el tipo del actor que colisiona con este
+		if (actorCollisioned instanceof Missile || actorCollisioned instanceof Bomb) {
+			// Si este actor colisiona con un misil o una bomba, debo eliminar el monstruo
+			this.setMarkedForRemoval(true);
+			// Si el monstruo colisiona con un misil o una bomba, también debo eliminar el mision o la bomba
+			actorCollisioned.setMarkedForRemoval(true);
+		}
+	}
+
+	public int getVx() {
+		return vx;
+	}
+
+	public void setVx(int vx) {
+		this.vx = vx;
+	}
+
+	public int getVy() {
+		return vy;
+	}
+
+	public void setVy(int vy) {
+		this.vy = vy;
+	}
+
+	public void paint(Graphics g) {
+		System.out.println("paint x: " + this.x + " y: " + this.y);
+		g.setColor(Color.white);
+		g.fillOval(this.x, this.y, 10, 10);
+	}
+
+	public static Pelota getInstance() {
+		if (instance == null) {
+			instance = new Pelota();
+		}
+		return instance;
+	}
+}
---
+++ Principal.java
@@ -0,0 +1,10 @@
+package version1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Arkanoid.getInstance().game();
+
+	}
+
+}
